aa9c3e49163244eec3106d8f1b02f3ee2446dd45
Tathagat Nawadia <hellotathagat@gmail.com>
2019-03-20 00:21:53 +0530

    Nothing unusual

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 print("Hello Gojek")
 
+class Registration_number():
+	delimiter = "-"
+	def __init__(self, registration_number):
+		self.segments = registration_number.split(Registration_number.delimiter)
+		self.registration_number = registration_number
+
+
+s = Registration_number("KA-23-22-33434")
+print(s.segments)
+
